7ac5b1585374a7b229e410600ea6f044631b9cb0
Jyry Suvilehto <jyrsa@iki.fi>
2014-07-26 12:06:51 +0000

    function to update beer info from Alko pages

--- beerstatus/tasks.py
+++ beerstatus/tasks.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.conf import settings
+from django.template.defaultfilters import slugify
 from django.utils import timezone
 from huey.djhuey import db_periodic_task
 from huey.djhuey import crontab
@@ -10,11 +11,13 @@ import requests
 import json
 import datetime
 from . import models
+from lxml import html
+from django.utils.timezone import utc
 
-LOGGER = logging.getLOGGER(__name__)
+LOGGER = logging.getLogger(__name__)
 
-@db_periodic_task(crontab(minute='*'))
-def refresh_beers():
+@db_periodic_task(crontab(hour='/3'))
+def refresh_beer_availability_statuses():
     LOGGER.info("starting beer status updates")
     for beer in models.Beer.objects.all().filter(active=True):
         for city_id, city in models.SUPPORTED_ALKO_CITIES:
@@ -25,7 +28,7 @@ def parse_alko_availability_date(value):
     #alko servers are always in helsinki local time so references like 
     #yesterday etc. also follow that
     helsinki = pytz.timezone("Europe/Helsinki")
-    date = datetime.datetime.now().astimezone(helsinki)
+    date = datetime.datetime.utcnow().replace(tzinfo=utc).astimezone(helsinki)
     if value == "today":
         return date
     if value == "yesterday":
@@ -84,3 +87,134 @@ def refresh_beer_availability(beer, cityID):
                                                 beer.name,
                                                 cityID,
                                                 all_available))
+
+def retrieve_etree(url):
+    """ returns an lxml etree for the url given
+    """
+    #i would love to just request the floats in en-US
+    #but alko doesn't honor the value consistently
+    response = requests.get(url )
+    tree = html.fromstring(response.text)
+    return tree
+
+def get_element_contents(tree, xpath, cure):
+    """ takes in the tree, xpath and a curing function that should clean the
+    value and/or coerce it into the right format (e.g. int(), float() or
+    a lambda).
+    
+    raises an error if no element is found. Returns first element if several
+    are found.
+    """
+    list_ = tree.xpath(xpath)
+    if len(list_) == 0:
+        raise ScrapeElementNotFoundException(
+            "element not found %s" % xpath
+            )
+    return cure( list_[0])
+
+def ScrapeInconsistencyException(BaseException):
+    pass
+
+
+def ScrapeElementNotFoundException(BaseException):
+    pass
+
+@db_periodic_task(crontab(hour='/12'))
+def update_beer_infos():
+    #loop through all after testing 
+    for beer in models.Beer.objects.all():
+        update_beer_info(beer)
+
+@db_task()
+def update_beer_info(beer):
+    """ retrieves beer page from alko, updates model fields and saves model.
+
+    Also retrieves name so that beer lists can be bootstrapped with just alko
+    product IDs in the future. Doesn't update name if one exists.
+    """
+    beer_url ="http://www.alko.fi/en/products/%s/" % beer.alko_product_id
+    tree = retrieve_etree(beer_url)
+    #be careful when retrieving these xpaths using chrome's dev tools
+    #chrome adds a tbody in the table that isn't present in the source and
+    #that lxml doesn't add
+    product_id = get_element_contents(
+                    tree,
+                    ('/html/body/div[7]/div/div[1]/div/div[1]/div[1]'
+                    '/div[2]/div[6]/table/tr[1]/td[2]/text()'),
+                    lambda x: x.strip()
+                ).strip()
+    if product_id != beer.alko_product_id:
+        raise ScrapInconsistencyException(("attempted to get beer info for"
+        "product %s but received info for product %s instead") %
+        (beer.alko_product_id, product_id))
+    product_category = get_element_contents(
+                    tree,
+                    ('/html/body/div[7]/div/div[1]/div/div[1]'
+                    '/div[1]/div[2]/div[6]/h3/text()'),
+                    lambda x: x.strip())
+    if product_category != "beer":
+        raise ScrapInconsistencyException(("attempted to get a beer but %s "
+        "is of category %s ") % (product_id, product_category))
+    abv = get_element_contents(
+                    tree,
+                    ('/html/body/div[7]/div/div[1]/div/div[1]/div[1]'
+                    '/div[2]/div[6]/table/tr[2]/td[2]/text()'),
+                    lambda x: float(x.replace('%', '').replace(",",".").strip())
+                    )
+    if abv < 0 or abv > 50:
+        raise ScrapInconsistencyException(("abv is not logica for a beer"
+        "produdct %d" % abv))
+    ebu = get_element_contents(
+                    tree,
+                    ('/html/body/div[7]/div/div[1]/div/div[1]/div[1]'
+                    '/div[2]/div[6]/table/tr[5]/td[2]/text()'),
+                    lambda x: float(x.replace('EBU', '').replace(",", ".").strip())
+                    )
+    if ebu < 0 :
+        raise ScrapInconsistencyException(("ebu is not logica for a beer"
+        "produdct %d" % ebu))
+    style =  get_element_contents(
+                    tree,
+                    ('/html/body/div[7]/div/div[1]/div/div[1]/div[1]'
+                    '/div[2]/div[6]/table/tr[10]/td[2]/text()'),
+                    lambda x: x.strip()
+                    )
+    #style info isn't in model yet so don't include it
+    price =  get_element_contents(
+                    tree,
+                    ('/html/body/div[7]/div/div[1]/div/div[1]/'
+                    'div[1]/div[2]/div[1]/span[1]/text()'),
+                    lambda x: float(x.strip().replace(",","."))
+                    )
+    if price <= 0: 
+        raise ScrapInconsistencyException(("beer price is <= 0."
+        " There's no such thing as free beer!"))
+    volume = get_element_contents(
+                    tree,
+                    ('/html/body/div[7]/div/div[1]/div/div[1]/'
+                    'div[1]/div[2]/div[2]/text()'),
+                    lambda x: float(x.strip().replace(",","."))
+                    )
+    if volume <= 0 or volume > 10:
+        raise ScrapInconsistencyException(("Beer volume is not credible!"
+        " There's no such thing as jottalitran tuopponen!"))
+    name = get_element_contents(
+                    tree,
+                    ('/html/body/div[7]/div/div[1]/div/'
+                    'div[1]/div[1]/div[2]/h1/text()'),
+                    lambda x: x.strip()
+                    )
+    if volume <= 0 or volume > 200:
+        raise ScrapInconsistencyException(("Beer name is incompatible: !"
+        " %" ) % name)
+    beer.abv = abv
+    beer.ebu = ebu
+    beer.price = price
+    beer.volume = volume
+    beer.style = style
+    if not beer.name:
+        beer.name = name
+        beer.slug = slugify(name) 
+    beer.save()
+
+
