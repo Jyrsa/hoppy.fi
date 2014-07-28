e9db3685e3d6c1b2a9e9e8424ec50f3214098397
Jyry Suvilehto <jyrsa@iki.fi>
2014-07-28 20:49:15 +0000

    move static pages to templates and simplify

--- beerstatus/urls.py
+++ beerstatus/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="beerstatus/index.html")),
+    url(r'^about/$', TemplateView.as_view(template_name="beerstatus/about.html")),
     url(r'^site.js$', views.site_js),
 ]
